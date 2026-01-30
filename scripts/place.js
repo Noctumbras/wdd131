f17be54a9bc1723d487b356369549c721196c8e1
Noctumbras <ranger.stephen@hotmail.com>
2026-01-30 16:27:48 -0600

    layout just about done

--- scripts/place.js
+++ scripts/place.js
@@ -2,18 +2,21 @@ const date = new Date();
 const windSpeed = 8;
 const temperature = 19;
 
-document.getElementById("currentyear").innerHTML = '\u00A9' + date.getFullYear();
-document.getElementById("lastModified").innerHTML = 'Last modified: ' + document.lastModified;
-
 function calculateWindChill(temperature, windSpeed) {
     let windChill = 35.74 + (0.6216 * temperature) - (35.75 * windSpeed ** 0.16) + (0.4275 * temperature * windSpeed ** 0.16);
     return windChill;
 }
 
+document.getElementById("temperature").innerHTML += temperature + "° F";
+document.getElementById("windSpeed").innerHTML += windSpeed + " mph";
+
 if (temperature <= 50 && windSpeed > 3) {
     let windChill = calculateWindChill(temperature, windSpeed);
-    document.getElementById("windChill").innerHTML += windChill;
+    document.getElementById("windChill").innerHTML += windChill + "° F";
 }
 else {
     document.getElementById("windChill").innerHTML += "N/A";
 }
+
+document.getElementById("currentyear").innerHTML = '\u00A9' + date.getFullYear();
+document.getElementById("lastModified").innerHTML = 'Last modified: ' + document.lastModified;
